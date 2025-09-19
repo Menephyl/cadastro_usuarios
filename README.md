3dfd893d9c6743ea5fbd63bec31abba1204ffaf0
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-09-19 18:42:18 -0300

    requisição api interna

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ![alt text](image.png)
+
+react hooks
+axios 
+api interna do nodenotes
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,39 +1,59 @@
-
-import { Button, Container, Inputs, Title, TopBackground, ContainerInputs, InputLabel, Form} from './styles.js'
+import { useRef } from 'react'
+import {
+  Button,
+  Container,
+  Inputs, Title,
+  TopBackground,
+  ContainerInputs,
+  InputLabel,
+  Form
+} from './styles.js'
 import UsersImage from '../../assets/cats.png'
+import api from '../../services/api.js'
 
 
 
-function App() {
+function Home() {
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
 
+  async function registerNewUser(){
+   const data = await api.post('/usuarios',{
+      email: inputEmail.current.value,
+      age: inputAge.current.value,
+      name: inputName.current.value
+    })
+    console.log(data)
+  }
   return (
     <>
       <Container>
         <TopBackground>
-         <img src={UsersImage} alt="cat" />
+          <img src={UsersImage} alt="cat" />
         </TopBackground>
         <Form>
           <Title>CADASTRO DE USUÁRIO</Title>
 
           <ContainerInputs>
-           
-              <div>
-                <InputLabel>Nome <span> * </span>
-                </InputLabel>
-                <Inputs type='text' placeholder=' Digite Nome' />
-              </div>
 
-              <div>
-                <InputLabel>Idade <span> * </span>
-                 </InputLabel>
-                  <Inputs type='text' placeholder=' Digite Idade' />
-              </div>
-            <div style={{width:'100%'}}>
-              <InputLabel>Contato <span> * </span>
-               </InputLabel>
-                <Inputs type='text' placeholder='  E-mail ou Whatsapp'/>
+            <div>
+              <InputLabel>Nome <span> * </span>
+              </InputLabel>
+              <Inputs type='text' placeholder=' Digite Nome'  ref={inputName} />
             </div>
-            <Button>
+
+            <div >
+              <InputLabel>Idade <span> * </span>
+              </InputLabel>
+              <Inputs type='text' placeholder=' Digite Idade' ref={inputAge} />
+            </div>
+            <div style={{ width: '100%' }}>
+              <InputLabel>Contato <span> * </span>
+              </InputLabel>
+              <Inputs type='text' placeholder='  E-mail ou Whatsapp'ref={inputEmail} />
+            </div>
+            <Button onClick={registerNewUser}>
               Cadastrar
 
             </Button>
@@ -45,4 +65,4 @@ function App() {
   )
 }
 
-export default App
+export default Home
